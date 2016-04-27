0923b3c3b4090bc2c2b33cea9d0dfd68c614e4df
janek_g <jmgrzesik@gmail.com>
2016-04-27 20:52:53 +0200

    Change for better dir

--- experiments/dla_windos.js
+++ experiments/dla_windos.js
@@ -1,7 +1,7 @@
 "use strict";
 var Client = require('./../src/client');
 var SocketMessenger = require("../src/socket_messenger");
-var directoryToWatch = '../testdir';
+var directoryToWatch = 'testdir';
 console.log("client watching directory: " + directoryToWatch);
 var socketMessenger = new SocketMessenger('192.168.99.1', 1234);
 new Client(directoryToWatch, socketMessenger);
--- experiments/dla_windos.ts
+++ experiments/dla_windos.ts
@@ -3,7 +3,7 @@
 import Client= require('./../src/client');
 import SocketMessenger = require("../src/socket_messenger");
 
-let directoryToWatch ='../testdir';
+let directoryToWatch ='testdir';
 console.log(`client watching directory: ${directoryToWatch}`);
 
 let socketMessenger = new SocketMessenger('192.168.99.1', 1234);
