45eb613f4eae0edcbbd0bbc72668f0c394898aea
janek_g <jmgrzesik@gmail.com>
2016-05-01 22:57:07 +0200

    Further cleanup

--- typings/browser.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="browser/ambient/async/index.d.ts" />
-/// <reference path="browser/ambient/commander/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/definitions/chalk/index.d.ts" />
-/// <reference path="browser/definitions/mkdirp/index.d.ts" />
-/// <reference path="browser/definitions/rimraf/index.d.ts" />
