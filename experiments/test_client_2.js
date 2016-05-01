3bd70529aef39ad345180b42be109f88246705ea
janek_g <jmgrzesik@gmail.com>
2016-05-01 22:56:09 +0200

    Cleanup in repository

--- experiments/test_client_2.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Client = require('./../src/client');
-var SocketMessenger = require("../src/socket_messenger");
-var directoryToWatch = '../testdir';
-console.log("client watching directory: " + directoryToWatch);
-var socketMessenger = new SocketMessenger('0.0.0.0', 1234);
-new Client(directoryToWatch, socketMessenger);
-//# sourceMappingURL=test_client_2.js.map
