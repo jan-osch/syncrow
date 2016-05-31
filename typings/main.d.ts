9d3ba41707b11c43d63dd3352ee4407966a29863
janek_g <jmgrzesik@gmail.com>
2016-05-31 20:52:57 +0200

    Adds typings

---
+++ typings/main.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="main/ambient/async/index.d.ts" />
+/// <reference path="main/ambient/commander/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />
+/// <reference path="main/definitions/chalk/index.d.ts" />
+/// <reference path="main/definitions/mkdirp/index.d.ts" />
+/// <reference path="main/definitions/rimraf/index.d.ts" />
