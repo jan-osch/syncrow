02dd848a444c01ecd594b3a4f2e76aac65e99919
janek_g <jmgrzesik@gmail.com>
2016-06-08 00:36:00 +0200

    Fixes some small bugs

--- src/server/bucket_router.ts
+++ src/server/bucket_router.ts
@@ -1,17 +1,21 @@
 import * as express from "express";
 import {BucketService} from "../bucket/bucket_service";
+import {debugFor} from "../utils/logger";
+
+const debug = debugFor('syncrow:bucket_router');
 
 export function bucketRouter(host:string, bucketService:BucketService) {
     const router = express.Router();
 
     router.get('/list', (req, res) => {
-
+        debug('requested bucket list');
         bucketService.getBucketsList((err, buckets)=> {
             res.json({buckets: buckets});
         });
     });
 
     router.get('/:bucketName/port', (req, res) => {
+        debug(`requested port for ${req.params.bucketName}`);
         bucketService.requestPortForBucket('', '', req.params.bucketName, (err, port)=> {
             res.json({
                 host: host,
